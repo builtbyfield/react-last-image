826d5209a9833c40a81133303cb91813a837eff5
Brett Klassen - iMac <brett@fieldmedialab.com>
2019-03-27 12:46:27 -0600

    1.0.1
    Initial functional goals:
    
    - Blur-up lazy loading from placeholders with ability to disable it.
    - Automatic generation of SrcSet
    - Hold aspect ratio or cover options
    - Fluid w/aspect ratio

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,102 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React from "react";
+import { render } from "react-dom";
 
-import Example from '../../src'
+import LastImage from "../../src";
 
-class Demo extends Component {
-  render() {
-    return <div>
-      <h1>last-image Demo</h1>
-      <Example/>
-    </div>
-  }
-}
+const Demo = () => (
+  <div>
+    <h1>Last Image</h1>
+    <LastImage
+      images={[
+        {
+          width: 2048,
+          height: 1536,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/5875b635908cc1aa27f0986cc81428ad0195f759_bryant.png"
+        },
+        {
+          width: 1024,
+          height: 768,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/27735deeaa46cb21157b7ba669b4991a66ad09b2_bryant.png"
+        },
+        {
+          width: 682,
+          height: 512,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/fc7395d3edc19b161b6b4269624ff27fa1fffa6d_bryant.png"
+        },
+        {
+          width: 20,
+          height: 15,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/50a2ec0236c49534f4fb15f392f917176b1be6b0_bryant.png"
+        }
+      ]}
+      alt="A photo of the last image I'll ever need"
+      style={{ height: "300px" }}
+      imgStyle={{ objectPosition: "top" }}
+    />
+    <LastImage
+      images={[
+        {
+          width: 2048,
+          height: 1536,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/06978ac6c87028703cb24ddc05ade59696c79e36_shannon-shaw-opener.jpg"
+        },
+        {
+          width: 1024,
+          height: 768,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/8b5a4114dd97712734431516541e649ba05ff6d3_shannon-shaw-opener.jpg"
+        },
+        {
+          width: 682,
+          height: 512,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/9b3a4240e55ea191c3ddc3877ef350ae1677f330_shannon-shaw-opener.jpg"
+        },
+        {
+          width: 20,
+          height: 15,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/2216048a53f4a82db21ac1d53811328bda450f4f_shannon-shaw-opener.jpg"
+        }
+      ]}
+      alt="A photo of the second last image I'll ever need"
+      lazy={false}
+    />
+    <LastImage
+      images={[
+        {
+          width: 2048,
+          height: 1536,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/e4d6004261af48deb076db58633d8bbe58e450d0_nekocase.jpg"
+        },
+        {
+          width: 1024,
+          height: 768,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/bcbcc4935117180b477a2d3d88f5a63f20b458f0_nekocase.jpg"
+        },
+        {
+          width: 682,
+          height: 512,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/8448cd7731a7802d229294cee04d3c91d8fb5d28_nekocase.jpg"
+        },
+        {
+          width: 20,
+          height: 15,
+          url:
+            "https://calgary-folk-music-festival.cdn.prismic.io/calgary-folk-music-festival/86c531a3f6d8e2f1954bfc9484dc4c38818e81c3_nekocase.jpg"
+        }
+      ]}
+      alt="A photo of the third last image I'll ever need"
+    />
+  </div>
+);
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,127 @@
-import React, {Component} from 'react'
+import React, { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import PropTypes from "prop-types";
+import "intersection-observer";
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+import "./styles.css";
+
+function LastImage({
+  images,
+  sizes,
+  breakpoints,
+  placeholder,
+  alt,
+  lazy,
+  style,
+  imgStyle
+}) {
+  const [isLoaded, setLoaded] = useState(false);
+  const [wrapperRef, isInView] = useInView();
+
+  const [placeholderImage, setPlaceholderImage] = useState(() => {
+    const placeholderImage = determinePlaceholder(images);
+    return placeholderImage;
+  });
+
+  function determinePlaceholder(images) {
+    let placeholderImage = images[0];
+    images.forEach(image => {
+      if (
+        image.width < placeholderImage.width ||
+        image.height < placeholderImage.height
+      ) {
+        placeholderImage = image;
+      }
+    });
+    return placeholderImage;
   }
+
+  const [imageSet, setImageSet] = useState(() => {
+    const imageSet = determineImageSet(images, breakpoints);
+    return imageSet;
+  });
+
+  function determineImageSet(images, breakpoints) {
+    let base = undefined;
+    let srcSet = "";
+    let sortedImages = images.sort(function(a, b) {
+      return a.width - b.width;
+    });
+    sortedImages.forEach((image, index) => {
+      if (image.width > breakpoints[0] && !base) {
+        base = image;
+      }
+      if (base) {
+        let closestBreak = 0;
+        breakpoints.forEach(point => {
+          if (point <= image.width) {
+            closestBreak = point;
+          }
+        });
+        srcSet +=
+          image.url +
+          " " +
+          closestBreak +
+          "w" +
+          (index !== sortedImages.length - 1 ? ", " : "");
+      }
+    });
+    return { base: base, srcSet: srcSet };
+  }
+
+  return (
+    <div className="last-image-wrapper" style={style} ref={wrapperRef}>
+      <div
+        style={{
+          width: "100%",
+          paddingBottom:
+            (imageSet.base.height / imageSet.base.width) * 100 + "%"
+        }}
+      />
+      {(!lazy || isInView || isLoaded) && (
+        <img
+          className="last-image"
+          style={imgStyle}
+          src={imageSet.base.url}
+          srcSet={imageSet.srcSet}
+          sizes={sizes}
+          alt={alt}
+          onLoad={() => {
+            setLoaded(true);
+          }}
+        />
+      )}
+      {!placeholder && (
+        <img
+          className={
+            "last-image last-image-placeholder" + (isLoaded && " fade")
+          }
+          style={imgStyle}
+          src={placeholderImage.url}
+        />
+      )}
+    </div>
+  );
 }
+
+LastImage.defaultProps = {
+  lazy: true,
+  breakpoints: [50, 512, 1024, 2048],
+  sizes: "(min-width: 800px) 50vw, 100vw"
+};
+
+LastImage.propTypes = {
+  images: PropTypes.shape([
+    {
+      url: PropTypes.string,
+      height: PropTypes.number,
+      width: PropTypes.number
+    }
+  ]).isRequired,
+  sizes: PropTypes.string,
+  breakpoints: PropTypes.array.isRequired,
+  alt: PropTypes.string,
+  lazy: PropTypes.bool
+};
+
+export default LastImage;
